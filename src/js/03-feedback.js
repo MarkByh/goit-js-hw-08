33156847c0700f9306a66defeead36e772fa8657
MarkByh <123231312fh@gmail.com>
2023-02-20 23:21:09 +0200

    tas3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,48 @@
+import throttle from "lodash.throttle";
 
+const form = document.querySelector('.feedback-form');
+const email = document.querySelector('input');
+const message = document.querySelector('textarea');
+const formKey = 'feedback-form-state';
+const formObj = {};
+
+forminputUpdate();
+
+const formInput = function(event) {
+    formObj[event.target.name] = event.target.value;
+    localStorage.setItem(formKey, JSON.stringify(formObj) )
+}
+
+
+const formSubmit = function(event) {
+    event.preventDefault();
+    
+    const {
+        elements: { email, message }
+    } = event.currentTarget;
+    
+    if (email.value == '' || message.value == '') {
+        return alert('Please fill in all the fields!!')
+    }
+    console.log(formObj);
+    event.currentTarget.reset();
+    // localStorage.removeItem(FEEDBACK_FORM);
+}
+
+form.addEventListener('input', throttle(formInput, 500));
+form.addEventListener('submit', formSubmit);
+
+
+
+
+
+function forminputUpdate() {
+const dataUpdate = JSON.parse(localStorage.getItem(formKey));
+    if(dataUpdate) {
+        console.log(dataUpdate.email || '');
+        console.log(dataUpdate.message || '');
+
+        email.value = dataUpdate.email || '';
+        message.value = dataUpdate.message || '';
+    }
+}
