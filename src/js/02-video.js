43422a45b1c7acefcc226f7c97a72dab8e6e02fa
MarkByh <123231312fh@gmail.com>
2023-02-20 17:49:20 +0200

    chenge

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,38 @@
+import Player from '@vimeo/player';
+import throttle from "lodash.throttle";
+const iframe = document.querySelector('iframe');
+const player = new Vimeo.Player(iframe);
+const VideoCurent = "videoplayer-current-time";
 
+player.on('play', function() {
+    console.log('played the video!');
+
+});
+
+player.getVideoTitle().then(function(title) {
+    console.log('title:', title);
+});
+
+const onPlay = function(data) {
+    // console.log(data.seconds);
+    const currentTime = data.seconds;
+    localStorage.setItem("videoplayer-current-time", currentTime);
+
+};
+player.on('timeupdate', onPlay);
+let TimeUpdate = localStorage.getItem(VideoCurent);
+
+player.setCurrentTime(TimeUpdate).then(function(seconds) {
+    // seconds = the actual time that the player seeked to
+}).catch(function(error) {
+    switch (error.name) {
+        case 'RangeError':
+            // the time was less than 0 or greater than the video’s duration
+            break;
+
+        default:
+            // some other error occurred
+            break;
+    }
+});
+player.on('timeupdate', throttle(onPlay, 1000)); 
